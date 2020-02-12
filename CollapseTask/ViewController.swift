3c3142676e78f230a20d0d3ff278869df61751cc
Mayoom <maryam_almoqbali@hotmail.com>
2020-02-12 13:03:32 +0400

    Initial Commit

---
+++ CollapseTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollapseTask
+//
+//  Created by Maryamalmoqbali on 2/12/20.
+//  Copyright © 2020 Maryamalmoqbali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
